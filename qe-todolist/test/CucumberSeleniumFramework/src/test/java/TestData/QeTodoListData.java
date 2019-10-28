c242fbd5ca8730f801ab853df537bf34ac443456
Dhashin <dhashinmoodley@yahoo.com>
2019-10-28 13:12:35 +0200

    Final change, reverting to localhost URL

--- qe-todolist/test/CucumberSeleniumFramework/src/test/java/TestData/QeTodoListData.java
+++ qe-todolist/test/CucumberSeleniumFramework/src/test/java/TestData/QeTodoListData.java
@@ -2,6 +2,6 @@ package TestData;
 
 public class QeTodoListData {
     //Application URl to access
-    public String applicationURL = "http://192.168.8.110:8080/todo";
+    public String applicationURL = "http://localhost:8081/todo";
 
 }
